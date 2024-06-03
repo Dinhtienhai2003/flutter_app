cf567b779bda496c9175c9cfc922b8b22e48955c
Dũng Prj <89343958+Dungprj@users.noreply.github.com>
2024-06-03 16:23:27 +0700

    them thong bao day

--- android/app/src/main/kotlin/com/example/thongbaoday/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/thongbaoday/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ontap_sharedprefences
+package com.example.thongbaoday
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,151 @@ import 'package:flutter/material.dart';
 import 'package:ontap_sharedprefences/provider/setting.dart';
 import 'package:ontap_sharedprefences/provider/task.dart';
 import 'package:provider/provider.dart';
+import 'package:ontap_sharedprefences/firebase_options.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'screen/home.dart';
 
-void main() async {
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  // await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await requestNotificationPermission();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Received message while app was in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      flutterLocalNotificationsPlugin.show(
+        message.hashCode,
+        message.notification!.title,
+        message.notification!.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            'channel_id',
+            'channel_name',
+            channelDescription: 'channel_description',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker',
+          ),
+        ),
+      );
+    }
+  });
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  // Lưu trữ fcmToken ở đây
+  print('Message data: ${fcmToken}');
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => SettingProvider()),
       ChangeNotifierProvider(create: (_) => TaskProvider()),
     ],
-    child: const MainApp(),
+    child: MyApp(),
   ));
 }
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+Future<void> requestNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  @override
-  State<MainApp> createState() => _MainAppState();
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
 }
 
-class _MainAppState extends State<MainApp> {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Push Notification Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainApp(),
+    );
+  }
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<MainApp> {
+  String _message = '';
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    _setupFirebaseMessaging();
+  }
+
+  void _setupFirebaseMessaging() {
+    FirebaseMessaging.instance.requestPermission();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Received message while app was in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        flutterLocalNotificationsPlugin.show(
+          message.hashCode,
+          message.notification!.title,
+          message.notification!.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              'channel_id',
+              'channel_name',
+              channelDescription: 'channel_description',
+              importance: Importance.max,
+              priority: Priority.high,
+              ticker: 'ticker',
+            ),
+          ),
+        );
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Received message while app was in the background!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        setState(() {
+          _message =
+              '${message.notification!.title}: ${message.notification!.body}';
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
